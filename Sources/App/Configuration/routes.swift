7bb2644263727543b5bcbd2309b5addcf104a8fa
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-28 15:53:06 -0500

    Created GET /*/billing/health route

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -1,4 +1,10 @@
 import Vapor
 
 /// Register your application's routes here.
-public func routes(_ router: Router) throws { }
+public func routes(_ router: Router) throws {
+    let root = router.grouped(any, "billing")
+
+    root.get("health") { request in
+        return "All good!"
+    }
+}
