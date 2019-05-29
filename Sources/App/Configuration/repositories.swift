b027266422b879770cc0ae53e2636189a212164c
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-29 16:40:06 -0500

    Registered DefaultUserRepository and DefaultSubscriptionRepository services

--- Sources/App/Configuration/repositories.swift
+++ Sources/App/Configuration/repositories.swift
@@ -3,4 +3,7 @@ import Service
 func repositories(services: inout Services)throws {
     let customers = CustomerRepositories()
     services.register(customers)
+
+    services.register(DefaultUserRepository.self)
+    services.register(DefaultSubscriptionRepository.self)
 }
