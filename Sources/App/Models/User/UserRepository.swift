b0ade6cc76e80fbe4b50ad7d34580a6f9f3e4d13
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-31 10:43:14 -0500

    Added .read(for:), .set(external:for:), and .delete(for:) methods to UserRepository protocol

--- Sources/App/Models/User/UserRepository.swift
+++ Sources/App/Models/User/UserRepository.swift
@@ -5,6 +5,10 @@ protocol UserRepository: ServiceType {
     func create(user: User) -> EventLoopFuture<User>
     func read(user: User.ID) -> EventLoopFuture<User?>
     func delete(user: User.ID) -> EventLoopFuture<Void>
+
+    func read(for customer: String) -> EventLoopFuture<User?>
+    func set(external: String, for customer: String) -> EventLoopFuture<User?>
+    func delete(for customer: String) -> EventLoopFuture<Void>
 }
 
 final class DefaultUserRepository: UserRepository {
@@ -34,9 +38,27 @@ final class DefaultUserRepository: UserRepository {
         }
     }
 
+    func read(for customer: String) -> EventLoopFuture<User?> {
+        return self.pool.withConnection { connection in
+            return User.query(on: connection).filter(\.customer == customer).first()
+        }
+    }
+
+    func set(external: String, for customer: String) -> EventLoopFuture<User?> {
+        return self.pool.withConnection { connection in
+            return User.query(on: connection).filter(\.customer == customer).update(\.external, to: external).first()
+        }
+    }
+
     func delete(user: User.ID) -> EventLoopFuture<Void> {
         return self.pool.withConnection { connection in
             return User.query(on: connection).filter(\.id == user).delete()
+        }
+    }
+
+    func delete(for customer: String) -> EventLoopFuture<Void> {
+        return self.pool.withConnection { connection in
+            return User.query(on: connection).filter(\.customer == customer).delete()
         }
     }
 }
