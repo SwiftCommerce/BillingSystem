2ea435d6c0a53aa48d08a44fe23eaf91cd8188e4
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-06-03 16:14:46 -0500

    Registered a ChargebeeConfig instance to the application services

--- Sources/App/Configuration/repositories.swift
+++ Sources/App/Configuration/repositories.swift
@@ -2,10 +2,15 @@ import Service
 
 func repositories(services: inout Services)throws {
     var customers = CustomerRepositories()
-    customers.register(.chargebee, repository: Chargebee.self)
+    chargebee(repositories: &customers, services: &services)
     services.register(customers)
 
     services.register(DefaultUserRepository.self)
     services.register(DefaultSubscriptionRepository.self)
     services.register(DefaultUserSubscriptionRepository.self)
+}
+
+func chargebee(repositories: inout CustomerRepositories, services: inout Services) {
+    services.register(ChargebeeConfig(apiKey: "", site: "skelpo-test"))
+    repositories.register(.chargebee, repository: Chargebee.self)
 }
